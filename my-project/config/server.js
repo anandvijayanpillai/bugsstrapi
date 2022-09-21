4e281f9127694662075d76df09cfbee744ee5553
anandvijayanpillai <anandvijay136@gmail.com>
2022-09-22 00:49:29 +0530

    newq commit

--- my-project/config/server.js
+++ my-project/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',["QNwDO28K/tUJ+Y6hv9Hafg==", "kdaaqE65wsIP2sTygdXPVA=="]),
   },
 });
